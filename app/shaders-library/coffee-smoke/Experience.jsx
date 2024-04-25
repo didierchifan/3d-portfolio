56fb2d5f6d0ecb0e0dcf1d952f637ae743d0595a
didierchifan <didier.chifan@gmail.com>
2024-04-25 09:13:45 +0300

    fixed the smoke

--- app/shaders-library/coffee-smoke/Experience.jsx
+++ app/shaders-library/coffee-smoke/Experience.jsx
@@ -1,17 +1,31 @@
-import { CameraControls, useGLTF, Center } from "@react-three/drei";
+import { CameraControls, useGLTF, Center, useTexture } from "@react-three/drei";
+
+import { useFrame } from "@react-three/fiber";
+
+import { useRef } from "react";
 
 import coffeeSmokeVertexShader from "../../shaders-glsl/coffeeSmoke/vertex.glsl";
 import coffeeSmokeFragmentShader from "../../shaders-glsl/coffeeSmoke/fragment.glsl";
 
-console.log(coffeeSmokeFragmentShader);
-console.log(coffeeSmokeVertexShader);
-
 import * as THREE from "three";
+
+const smokeGeometry = new THREE.PlaneGeometry(1, 1, 16, 64);
+smokeGeometry.translate(0, 0.5, 0);
+smokeGeometry.scale(1.5, 6, 1.5);
 
 export default function Experience() {
   const { nodes } = useGLTF("../3dModels/bakedModel.glb");
+  const perlinTexture = useTexture("../textures/coffee-smoke/perlin.png");
+  perlinTexture.wrapS = THREE.RepeatWrapping;
+  perlinTexture.wrapT = THREE.RepeatWrapping;
 
   const mesh = nodes.baked;
+
+  const coffeeSmokeMaterial = useRef();
+
+  useFrame((state, delta) => {
+    coffeeSmokeMaterial.current.uniforms.uTime.value += delta;
+  });
 
   return (
     <>
@@ -21,9 +35,19 @@ export default function Experience() {
       <Center>
         <mesh geometry={mesh.geometry} material={mesh.material} />
 
-        <mesh scale={[1.5, 3, 1.5]} position={[0, 1.5, 0]}>
-          <planeGeometry args={[1, 1, 16, 64]}></planeGeometry>
-          <shaderMaterial side={THREE.DoubleSide} />
+        <mesh geometry={smokeGeometry}>
+          <shaderMaterial
+            ref={coffeeSmokeMaterial}
+            vertexShader={coffeeSmokeVertexShader}
+            fragmentShader={coffeeSmokeFragmentShader}
+            transparent={true}
+            depthWrite={false}
+            uniforms={{
+              uTime: { value: 0 },
+              uPerlinTexture: new THREE.Uniform(perlinTexture),
+            }}
+            side={THREE.DoubleSide}
+          />
         </mesh>
       </Center>
     </>
