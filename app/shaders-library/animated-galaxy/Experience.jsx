4b0e993086c4b19df87b652d2c4e37fe30b7241f
didierchifan <didier.chifan@gmail.com>
2024-05-22 12:53:22 +0300

    implemented the galaxy generator

--- app/shaders-library/animated-galaxy/Experience.jsx
+++ app/shaders-library/animated-galaxy/Experience.jsx
@@ -1,21 +1,135 @@
 "use client";
-import {
-  shaderMaterial,
-  CameraControls,
-  useGLTF,
-  Center,
-} from "@react-three/drei";
-import { BoxGeometry } from "three";
+
+import * as THREE from "three";
+import { shaderMaterial, CameraControls, Center } from "@react-three/drei";
+import useGuiControls from "./hooks/useGuiControls";
+import { useFrame } from "@react-three/fiber";
+
+import { useControls } from "leva";
+
+import { useEffect, useMemo, useRef } from "react";
+import { random } from "gsap";
+import { color } from "framer-motion";
 
 export default function Experience() {
+  const {
+    count,
+    size,
+    radius,
+    branches,
+    spin,
+    randomness,
+    randomnessPower,
+    insideColor,
+    outsideColor,
+  } = useGuiControls();
+
+  const pointsRef = useRef(null);
+
+  const { positions, colors } = useMemo(() => {
+    const positions = new Float32Array(count * 3);
+    const colors = new Float32Array(count * 3);
+
+    const colorInside = new THREE.Color(insideColor);
+    const colorOutside = new THREE.Color(outsideColor);
+
+    for (let i = 0; i < count; i++) {
+      const i3 = i * 3;
+
+      //particles positions attribute
+      const randomRadius = Math.random() * radius;
+      const spinAngle = randomRadius * spin;
+      const branchAngle = ((i % branches) / branches) * Math.PI * 2;
+
+      const randomX =
+        Math.pow(Math.random(), randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1) *
+        randomness *
+        randomRadius;
+      const randomY =
+        Math.pow(Math.random(), randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1) *
+        randomness *
+        randomRadius;
+      const randomZ =
+        Math.pow(Math.random(), randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1) *
+        randomness *
+        randomRadius;
+
+      positions[i3] =
+        Math.cos(branchAngle + spinAngle) * randomRadius + randomX;
+      positions[i3 + 1] = randomY;
+      positions[i3 + 2] =
+        Math.sin(branchAngle + spinAngle) * randomRadius + randomZ;
+
+      //particles colors attribute
+
+      const mixedColor = colorInside.clone();
+      mixedColor.lerp(colorOutside, randomRadius / radius);
+
+      colors[i3] = mixedColor.r;
+      colors[i3 + 1] = mixedColor.g;
+      colors[i3 + 2] = mixedColor.b;
+    }
+
+    return { positions, colors };
+  }, [
+    count,
+    radius,
+    branches,
+    spin,
+    randomness,
+    randomnessPower,
+    outsideColor,
+    insideColor,
+  ]);
+
+  // We use useEffect to update the geometry attribute whenever the positions array changes => this ensures that the geometry is updated correctly.
+  useEffect(() => {
+    if (pointsRef.current) {
+      pointsRef.current.geometry.setAttribute(
+        "position",
+        new THREE.BufferAttribute(positions, 3)
+      );
+
+      pointsRef.current.geometry.setAttribute(
+        "color",
+        new THREE.BufferAttribute(colors, 3)
+      );
+      //very important to set the needUpdate to true
+      pointsRef.current.geometry.attributes.position.needsUpdate = true;
+      pointsRef.current.geometry.attributes.color.needsUpdate = true;
+    }
+  }, [positions, colors]);
+
   return (
     <>
       <CameraControls />
       <Center>
-        <mesh>
-          <boxGeometry />
-          <meshBasicMaterial />
-        </mesh>
+        <points ref={pointsRef}>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              count={count}
+              array={positions}
+              itemSize={3}
+            />
+            <bufferAttribute
+              attach="attributes-color"
+              count={count}
+              array={colors}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <pointsMaterial
+            size={size}
+            sizeAttenuation
+            depthWrite={false}
+            blending={THREE.AdditiveBlending}
+            vertexColors={true}
+          />
+        </points>
       </Center>
     </>
   );
--- app/shaders-library/animated-galaxy/hooks/useGuiControls.jsx
+++ app/shaders-library/animated-galaxy/hooks/useGuiControls.jsx
@@ -1,48 +1,51 @@
-"use client";
 import { useControls } from "leva";
 
 export default function useGuiControls() {
-  const controls = useControls("Galaxy", {
+  const controls = useControls("Galaxy configurator", {
     count: {
-      value: 200000,
-      min: 100,
-      max: 1000000,
-      step: 100,
+      value: 20000,
+      min: 1,
+      max: 100000,
+      step: 1,
     },
-
+    size: {
+      value: 0.02,
+      min: 0.001,
+      max: 0.1,
+    },
     radius: {
-      value: 5,
-      min: 0.01,
-      max: 20,
-      step: 0.01,
+      value: 10,
+      min: 0.1,
+      max: 30,
     },
-
     branches: {
       value: 3,
       min: 2,
       max: 20,
       step: 1,
     },
-
+    spin: {
+      value: 1,
+      min: -5,
+      max: 5,
+    },
     randomness: {
       value: 0.2,
       min: 0,
       max: 2,
-      step: 0.001,
+      step: 0.01,
     },
-
     randomnessPower: {
       value: 3,
-      min: 0,
+      min: 0.1,
       max: 10,
-      step: 0.001,
+      step: 1,
     },
-
     insideColor: {
       value: "#ff6030",
     },
     outsideColor: {
-      value: "#4076ff",
+      value: "#5786F5",
     },
   });
 
