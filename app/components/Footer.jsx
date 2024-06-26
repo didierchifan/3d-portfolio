da292fa26afd3c9fa6a65eab0fb7c50e914d2fa0
didierchifan <didier.chifan@gmail.com>
2024-06-26 12:07:43 +0300

    fixed the footer's height (550px)

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,14 +4,14 @@ import Content from "./Content";
 export default function Footer({ backgroundColor, color, fill }) {
   return (
     <div
-      className="relative h-[400px]"
+      className="relative h-[550px]"
       style={{
         clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)",
         backgroundColor,
       }}
     >
-      <div className="relative h-[calc(100vh+400px)] -top-[100vh]">
-        <div className="h-[400px] sticky top-[calc(100vh-400px)]">
+      <div className="relative h-[calc(100vh+550px)] -top-[100vh]">
+        <div className="h-[550px] sticky top-[calc(100vh-550px)]">
           <Content
             backgroundColor={backgroundColor}
             color={color}
