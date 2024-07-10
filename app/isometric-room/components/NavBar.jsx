6598f819cc9636926df5cf979d455dc4f7c1e871
didierchifan <didier.chifan@gmail.com>
2024-07-10 12:54:43 +0300

    implemented sounds for all the interactive buttons

--- app/isometric-room/components/NavBar.jsx
+++ app/isometric-room/components/NavBar.jsx
@@ -72,15 +72,68 @@ export default function Navigation() {
   };
 
   // simple sound on click
-  const playSound = () => {
+  const playNeon = () => {
     const sound = neon.current;
     if (sound) {
       sound.play().catch((error) => console.log("Playback prevented ", error));
     }
   };
 
-  const handlePaintLampLightandPlaySound = (e) => {
-    handlePaintLampLight(e), playSound(e);
+  const handleAmbientLightandPlaySound = (e) => {
+    handleAmbientLightClick(e), playNeon(e);
+  };
+
+  const donut = useRef();
+  const playDonut = () => {
+    const sound = donut.current;
+    if (sound) {
+      sound.play().catch((error) => console.log("Playback prevented", error));
+    }
+  };
+
+  const handleDonutLightandPlayDonut = (e) => {
+    handleDonutLightClick(e), playDonut(e);
+  };
+
+  const chairSound = useRef();
+  const playChair = () => {
+    const sound = chairSound.current;
+    if (sound) {
+      sound.play().catch((error) => console.log("Playbacik prevented:", error));
+    }
+  };
+
+  const tvSound = useRef();
+  const playTvSound = () => {
+    const sound = tvSound.current;
+    if (sound) {
+      sound.play().catch((error) => console.log("Playback prevented:", erro));
+    }
+  };
+  const handleTvAndPlayTvSound = (e) => {
+    handleTvLight(e), playTvSound(e);
+  };
+
+  const wallLampSound = useRef();
+  const playWallLamp = () => {
+    const sound = wallLampSound.current;
+    if (sound)
+      sound.play().catch((error) => console.log("Playback prevented:", error));
+  };
+
+  const handleWallLampAndPlayWallLampSound = (e) => {
+    handlePaintLampLight(e), playWallLamp();
+  };
+
+  const lampSound = useRef();
+  const playLampSound = () => {
+    const sound = lampSound.current;
+    if (sound) {
+      sound.play().catch((error) => console.log("Playback prevented:", error));
+    }
+  };
+  const handleLampSoundandPlayLampSound = (e) => {
+    handleLampLight(e), playLampSound(e);
   };
 
   return (
@@ -124,7 +177,7 @@ export default function Navigation() {
             onClick={playOnClick}
           >
             <audio ref={shadersSound}>
-              <source src="sounds/ambient.mp3" type="audio/wav" />
+              <source src="sounds/shaders-sound.wav" type="audio/wav" />
               Your browser does not support the audio file.
             </audio>
             <Link href="./shaders-library">
@@ -139,55 +192,80 @@ export default function Navigation() {
             // style={{ backgroundColor: "#F5F5F7" }}
             data-tooltip="Light Switch"
             className="tooltip-container bg-white hover:bg-orange-500 w-12 h-12 rounded-md flex items-center justify-center"
-            onClick={handleAmbientLightClick}
+            onClick={handleAmbientLightandPlaySound}
           >
+            <audio ref={neon}>
+              <source src="sounds/neon.wav" type="audio/wav" />
+              Your browser does not support the audio file.
+            </audio>
+
             <MainLight fill="#181818" className="w-10 h-10" />
           </div>
           <div
             // style={{ backgroundColor: "#F5F5F7" }}
             data-tooltip="Donut Lamp"
             className="tooltip-container bg-white hover:bg-orange-500 w-12 h-12  rounded-md flex items-center justify-center "
-            onClick={handleDonutLightClick}
+            onClick={handleDonutLightandPlayDonut}
           >
+            <audio ref={donut}>
+              <source src="sounds/donut.wav" type="audio/wav" />
+              Your browser does not support the audio file.
+            </audio>
+
             <DonutLight fill="#181818" className="w-10 h-10" />
           </div>
           <div
             // style={{ backgroundColor: "#F5F5F7" }}
             data-tooltip="Akja Lamp"
             className="tooltip-container bg-white hover:bg-orange-500 w-12 h-12  rounded-md flex items-center justify-center "
-            onClick={handleLampLight}
+            onClick={handleLampSoundandPlayLampSound}
           >
+            <audio ref={lampSound}>
+              <source src="sounds/ackja.wav" type="audio/wav" />
+              Your browser does not support the audio file.
+            </audio>
             <AckjaLight fill="#181818" className="w-10 h-10" />
           </div>
           <div
             // style={{ backgroundColor: "#F5F5F7" }}
             data-tooltip="Wall Lamp"
             className="tooltip-container bg-white hover:bg-orange-500 w-12 h-12  rounded-md flex items-center justify-center "
-            onClick={handlePaintLampLightandPlaySound}
+            onClick={handleWallLampAndPlayWallLampSound}
           >
-            <audio ref={neon}>
-              <source src="sounds/neon.wav" type="audio/wav" />
+            <audio ref={wallLampSound}>
+              <source src="sounds/wall-lamp.wav" type="audio/wav" />
               Your browser does not support the audio file.
             </audio>
             <WallLight fill="#181818" className="w-10 h-10 " />
           </div>
+
           <div
             // style={{ backgroundColor: "#F5F5F7" }}
             data-tooltip="TV Ambient Light"
             className="tooltip-container bg-white hover:bg-orange-500 w-12 h-12  rounded-md flex items-center justify-center "
-            onClick={handleTvLight}
+            onClick={handleTvAndPlayTvSound}
           >
+            <audio ref={tvSound}>
+              <source src="sounds/tv.wav" type="audio/wav" />
+              Your browser does not support the audio file.
+            </audio>
             <TvLight fill="#181818" className="w-10 h-10 " />
           </div>
+
           <div
             // style={{ backgroundColor: "#F5F5F7" }}
             data-tooltip="Have a sit!"
             className="tooltip-container bg-white hover:bg-orange-500 w-12 h-12 rounded-md flex items-center justify-center"
             onClick={() => {
-              console.log("clickcassss");
               document.dispatchEvent(takeASeatEvent);
+              playChair();
             }}
           >
+            <audio ref={chairSound}>
+              <source src="sounds/chair.wav" type="audio/wav" />
+              Your browser does not support the audio file.
+            </audio>
+
             <ChairNav fill="#181818" className="w-10 h-10 " />
           </div>
         </div>
